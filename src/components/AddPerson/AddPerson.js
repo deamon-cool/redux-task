b89e302a87c07995eb7de75083efe7acac639cbc
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-22 19:58:15 +0200

    added handling inputs methods

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -4,14 +4,23 @@ import './AddPerson.css';
 
 class AddPerson extends React.Component {
     state = {
-
+        name: '',
+        age: null
     };
+
+    nameChangedHandler = (e) => {
+        this.setState({ name: e.target.value });
+    }
+
+    ageChangeHandler = (e) => {
+        this.setState({ age: e.target.value });
+    }
 
     render() {
         return (
             <div className="AddPerson">
-                <input type='text' placeholder='Name' />
-                <input type='number' placeholder='Age' />
+                <input type='text' placeholder='Name' onChange={this.nameChangedHandler} />
+                <input type='number' placeholder='Age' onChange={this.ageChangeHandler} />
                 <button onClick={this.props.personAdded}>Add Person</button>
             </div>
         );
